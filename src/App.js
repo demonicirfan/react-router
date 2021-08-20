4caa96caf8e24af30f62a773d2fbfab64126fed5
demonicirfan <hashtag.irfan@gmail.com>
2021-08-20 13:34:39 +0530

    proctected route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Redirect, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header";
 import About from "./Pages/About";
@@ -6,19 +6,26 @@ import Error from "./Pages/Error";
 import Home from "./Pages/Home";
 import Path from "./Pages/Path";
 import Profile from "./Pages/Profile";
+import React, { useState } from "react";
 
 function App() {
+  const [login, setLogin] = useState(false);
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
+        <button onClick={() => setLogin(!login)}>
+          {login ? "logout" : "login"}
+        </button>
       </div>
 
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} exact />
-      <Route path="/profile" component={Profile} exact />
+      <Route path="/profile" exact>
+        {login ? <Profile /> : <Redirect to="/" />}
+      </Route>
       <Route component={Error} exact />
-      <Route path="/path/:id" component={Path}/>
+      <Route path="/path/:id" component={Path} />
     </BrowserRouter>
   );
 }
